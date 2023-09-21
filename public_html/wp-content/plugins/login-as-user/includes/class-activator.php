fea0f2fe0f34995806efdaeb2aea1a73e370617e
Server <server@admin.com>
2023-09-21 21:53:01 +0330

    install login as user plugin

---
+++ public_html/wp-content/plugins/login-as-user/includes/class-activator.php
@@ -0,0 +1,23 @@
+<?php
+/* ======================================================
+ # Login as User for WordPress - v1.4.7 (free version)
+ # -------------------------------------------------------
+ # For WordPress
+ # Author: Web357
+ # Copyright @ 2014-2023 Web357. All rights reserved.
+ # License: GNU/GPLv3, http://www.gnu.org/licenses/gpl-3.0.html
+ # Website: https:/www.web357.com
+ # Demo: https://demo.web357.com/wordpress/login-as-user/wp-admin/
+ # Support: support@web357.com
+ # Last modified: Thursday 27 July 2023, 03:31:06 AM
+ ========================================================= */
+/**
+ * Fired during plugin activation
+ */
+class LoginAsUser_Activator {
+
+	public static function activate() {
+
+	}
+
+}
---
+++ public_html/wp-content/plugins/login-as-user/public/js/public.js
@@ -0,0 +1,21 @@
+/* ======================================================
+ # Login as User for WordPress - v1.4.7 (free version)
+ # -------------------------------------------------------
+ # For WordPress
+ # Author: Web357
+ # Copyright @ 2014-2023 Web357. All rights reserved.
+ # License: GNU/GPLv3, http://www.gnu.org/licenses/gpl-3.0.html
+ # Website: https:/www.web357.com
+ # Demo: https://demo.web357.com/wordpress/login-as-user/wp-admin/
+ # Support: support@web357.com
+ # Last modified: Thursday 27 July 2023, 03:31:06 AM
+ ========================================================= */
+jQuery(function ($) {
+	'use strict';
+
+	$('.w357-login-as-user-btn').bind('contextmenu', function(e) {
+		alert('The right click is disabled. Please, just click on the button.');
+		return false;
+	}); 
+
+});
