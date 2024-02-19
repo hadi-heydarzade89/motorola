78493e6f405a44c9fa8507811ce4144757a89ea9
Server <server@admin.com>
2024-02-19 14:12:48 +0330

    Remove old parsian payment gateway plugin

--- public_html/wp-config.php
+++ public_html/wp-config.php
@@ -77,8 +77,8 @@ $table_prefix = 'wp_';
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
 /* Add any custom values between this line and the "stop editing" line. */
-define( 'WP_DEBUG', false );
-define( 'WP_DEBUG_LOG', false );
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
 define( 'WP_DEBUG_DISPLAY', false );
 
 #@ini_set( 'display_errors', 0 );
