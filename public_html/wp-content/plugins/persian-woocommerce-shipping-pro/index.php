79cac6854327c779f21d8417d1f4374c7e0225f2
Server <server@admin.com>
2024-07-20 16:21:51 +0330

    Remove persian-woocommerce-shipping and persian-woocommerce-shipping-pro plugins

--- public_html/wp-content/plugins/persian-woocommerce-shipping-pro/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Developer :         Nabik
- * Web Site  :         Nabik.Net
- * E-Mail    : Contact@Nabik.Net
- * أَلسَّلٰامُ عَلَیکَ یٰا عَلی اِبنِ موسَی أَلرّضٰآ
- */
-?>
-تا اینجا اومدی یه صلوات بفرست
